55e462c3921f129508576b3e89f0952ebdf733e1
Qiu105 <48044740+Qiu105@users.noreply.github.com>
2022-04-21 16:49:25 +0800

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -4,31 +4,20 @@ import os
 import os.path
 
 
-def has_file_allowed_extension(filename, extensions):
-    """Checks if a file is an allowed extension.
+IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']
 
-    Args:
-        filename (string): path to a file
 
-    Returns:
-        bool: True if the filename ends with a known image extension
-    """
+def allowed_extension(filename, extensions):
     filename_lower = filename.lower()
-    return any(filename_lower.endswith(ext) for ext in extensions)
+    flag = any(filename_lower.endswith(ext) for ext in extensions)
+    return flag
 
 
-def find_classes(dir):
-    classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
-    classes.sort()
-    class_to_idx = {classes[i]: i for i in range(len(classes))}
-    return classes, class_to_idx
-
-
-def make_dataset(dir, extensions):
+def create_dataset(dir, extensions):
     images = []
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in sorted(fnames):
-            if has_file_allowed_extension(fname, extensions):
+            if allowed_extension(fname, extensions):
                 path = os.path.join(root, fname)
                 item = (path, 0)
                 images.append(item)
@@ -38,28 +27,19 @@ def make_dataset(dir, extensions):
 
 class DatasetFolder(data.Dataset):
     def __init__(self, root, loader, extensions, transform=None, target_transform=None):
-        # classes, class_to_idx = find_classes(root)
-        samples = make_dataset(root, extensions)
+        samples = create_dataset(root, extensions)
         if len(samples) == 0:
-            raise(RuntimeError("Found 0 files in subfolders of: " + root + "\n"
-                               "Supported extensions are: " + ",".join(extensions)))
+            raise(RuntimeError("未发现文件！"))
 
         self.root = root
         self.loader = loader
         self.extensions = extensions
         self.samples = samples
-
         self.transform = transform
         self.target_transform = target_transform
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
 
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        """
         path, target = self.samples[index]
         sample = self.loader(path)
         if self.transform is not None:
@@ -72,35 +52,15 @@ class DatasetFolder(data.Dataset):
     def __len__(self):
         return len(self.samples)
 
-    def __repr__(self):
-        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
-        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
-        fmt_str += '    Root Location: {}\n'.format(self.root)
-        tmp = '    Transforms (if any): '
-        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        tmp = '    Target Transforms (if any): '
-        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        return fmt_str
 
-
-IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']
-
-
-def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+def loader(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
 
 
-def default_loader(path):
-    return pil_loader(path)
-
-
 class ImageFolder(DatasetFolder):
     def __init__(self, root, transform=None, target_transform=None,
-                 loader=default_loader):
-        super(ImageFolder, self).__init__(root, loader, IMG_EXTENSIONS,
-                                          transform=transform,
-                                          target_transform=target_transform)
+                 loader=loader):
+        super(ImageFolder, self).__init__(root, loader, IMG_EXTENSIONS, transform=transform, target_transform=target_transform)
         self.imgs = self.samples
