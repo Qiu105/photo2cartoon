8b99b41c7ef4ab497a4de35321ab524995f4df07
Qiu105 <48044740+Qiu105@users.noreply.github.com>
2022-04-21 16:44:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,52 +4,17 @@ import shutil
 from utils.utils import *
 
 def parse_args():
-    """parsing and configuration"""
-    desc = "photo2cartoon"
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--phase', type=str, default='train', help='[train / test]')
-    parser.add_argument('--light', type=str2bool, default=True, help='[U-GAT-IT full version / U-GAT-IT light version]')
+    parser = argparse.ArgumentParser(description="photo2cartoon")
+    parser.add_argument('--mode', type=str, default='train', help='[train / test]')
     parser.add_argument('--dataset', type=str, default='photo2cartoon', help='dataset name')
-
-    parser.add_argument('--iteration', type=int, default=1000000, help='The number of training iterations')
+    parser.add_argument('--epoch', type=int, default=10,help='The number of training epochs')
+    parser.add_argument('--iteration', type=int, default=10000, help='The number of training iterations')
     parser.add_argument('--batch_size', type=int, default=1, help='The size of batch size')
-    parser.add_argument('--print_freq', type=int, default=1000, help='The number of image print freq')
-    parser.add_argument('--save_freq', type=int, default=1000, help='The number of model save freq')
     parser.add_argument('--decay_flag', type=str2bool, default=True, help='The decay_flag')
-
     parser.add_argument('--lr', type=float, default=0.0001, help='The learning rate')
-    parser.add_argument('--adv_weight', type=int, default=1, help='Weight for GAN')
-    parser.add_argument('--cycle_weight', type=int, default=50, help='Weight for Cycle')
-    parser.add_argument('--identity_weight', type=int, default=10, help='Weight for Identity')
-    parser.add_argument('--cam_weight', type=int, default=1000, help='Weight for CAM')
-    parser.add_argument('--faceid_weight', type=int, default=1, help='Weight for Face ID')
-
-    parser.add_argument('--ch', type=int, default=32, help='base channel number per layer')
-    parser.add_argument('--n_dis', type=int, default=6, help='The number of discriminator layer')
-
-    parser.add_argument('--img_size', type=int, default=256, help='The size of image')
-    parser.add_argument('--img_ch', type=int, default=3, help='The size of image channel')
-
-    parser.add_argument('--device', type=str, default='cuda:0', help='Set gpu mode: [cpu, cuda]')
-    parser.add_argument('--gpu_ids', type=int, default=[0], nargs='+', help='Set [0, 1, 2, 3] for multi-gpu training')
-    parser.add_argument('--benchmark_flag', type=str2bool, default=False)
-    parser.add_argument('--resume', type=str2bool, default=False)
-    parser.add_argument('--rho_clipper', type=float, default=1.0)
-    parser.add_argument('--w_clipper', type=float, default=1.0)
-    parser.add_argument('--pretrained_weights', type=str, default='', help='pretrained weight path')
-
+    parser.add_argument('--device', type=str, default='cpu', help='Set gpu mode: [cpu, cuda]')
     args = parser.parse_args()
-    args.result_dir = './experiment/{}-size{}-ch{}-{}-lr{}-adv{}-cyc{}-id{}-identity{}-cam{}'.format(
-        os.path.basename(__file__)[:-3],
-        args.img_size,
-        args.ch,
-        args.light,
-        args.lr,
-        args.adv_weight,
-        args.cycle_weight,
-        args.faceid_weight,
-        args.identity_weight,
-        args.cam_weight)
+    args.result_dir = './ex'
 
     return check_args(args)
 
@@ -66,15 +31,15 @@ def main():
     if args is None:
         exit()
 
-    gan = Photo2Cartoon(args)
-    gan.build_model()
+    net = Photo2Cartoon(args)
+    net.build_model()
 
-    if args.phase == 'train':
-        gan.train()
-        print(" [*] Training finished!")
+    if args.mode == 'train':
+        net.train()
+        print(" [*] Train finished!")
 
-    if args.phase == 'test':
-        gan.test()
+    if args.mode == 'test':
+        net.test()
         print(" [*] Test finished!")
 
 
